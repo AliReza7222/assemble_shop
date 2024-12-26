7319e95a3eee0a5619f72e26faa8f462c4f29d54
AliReza7222 <ali.r.salehi99@gmail.com>
2024-12-26 11:40:12 +0330

    doc string for get_items_with_price_based_quantity and get_total_price_order was written

--- assemble_shop/orders/utils.py
+++ assemble_shop/orders/utils.py
@@ -1,10 +1,25 @@
 from decimal import Decimal
 
-from django.db.models import Case, DecimalField, ExpressionWrapper, F, Sum, When
+from django.db.models import (
+    Case,
+    DecimalField,
+    ExpressionWrapper,
+    F,
+    QuerySet,
+    Sum,
+    When,
+)
 from django.utils import timezone
 
+from assemble_shop.orders.models import Order
 
-def get_items_with_price_based_quantity(order):
+
+def get_items_with_price_based_quantity(order: Order) -> QuerySet:
+    """
+    Annotates each order item with its effective price after applying any active discount
+    and calculates the total item price based on quantity. The calculation is done at the
+    database level using `Case`, `When`, and `F` expressions to optimize performance.
+    """
     return order.items.annotate(
         effective_price=Case(
             When(
@@ -25,7 +40,11 @@ def get_items_with_price_based_quantity(order):
     )
 
 
-def get_total_price_order(order):
+def get_total_price_order(order: Order) -> Decimal:
+    """
+    Calculates the total price of an order by summing up the total price of each item,
+    considering quantity and any discount at the database.
+    """
     items = get_items_with_price_based_quantity(order)
     total = items.aggregate(total=Sum("total_item_price"))["total"]
     return total.quantize(Decimal("0.01")) if total else Decimal("0.00")

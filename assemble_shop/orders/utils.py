c3129e71649968f07c27c260f60c24534a244be0
AliReza7222 <ali.r.salehi99@gmail.com>
2024-12-28 00:21:02 +0330

    Signals were optimized and the problem of updating discounts on orders was fixed

--- assemble_shop/orders/utils.py
+++ assemble_shop/orders/utils.py
@@ -5,39 +5,53 @@ from django.db.models import (
     DecimalField,
     ExpressionWrapper,
     F,
+    OuterRef,
     QuerySet,
+    Subquery,
     Sum,
     When,
 )
 from django.utils import timezone
 
 from assemble_shop.orders.enums import OrderStatusEnum
-from assemble_shop.orders.models import Order, OrderItem
+from assemble_shop.orders.models import Discount, Order, OrderItem, Product
 
 
-def get_items_with_price_based_quantity(order: Order) -> QuerySet:
+def _get_active_discount_subquery():
+    """
+    Returns a subquery to fetch the active discount percentage for a product.
+    This subquery ensures that only active discounts within the current time range are considered.
+    """
+    return Discount.objects.filter(
+        product=OuterRef("product_id"),
+        is_active=True,
+        start_date__lte=timezone.now(),
+        end_date__gte=timezone.now(),
+    ).values("discount_percentage")[:1]
+
+
+def _get_items_price_based_on_quantity(order: Order) -> QuerySet:
     """
     Annotates each order item with its effective price after applying any active discount
-    and calculates the total item price based on quantity. The calculation is done at the
-    database level using `Case`, `When`, and `F` expressions to optimize performance.
+    and calculates the total item price based on quantity.
     """
+    discount_subquery = _get_active_discount_subquery()
+
     return order.items.annotate(
-        effective_price=Case(
-            When(
-                product__discounts__start_date__lte=timezone.now(),
-                product__discounts__end_date__gte=timezone.now(),
-                product__discounts__is_active=True,
-                then=F("product__price")
-                * (1 - F("product__discounts__discount_percentage") / 100),
-            ),
-            default=F("product__price"),
-            output_field=DecimalField(max_digits=10, decimal_places=2),
-        )
-    ).annotate(
+        discount_percentage_now=Subquery(discount_subquery),
         total_item_price=ExpressionWrapper(
-            F("quantity") * F("effective_price"),
+            F("quantity")
+            * Case(
+                When(
+                    discount_percentage_now__isnull=False,
+                    then=F("product__price")
+                    * (1 - F("discount_percentage_now") / 100),
+                ),
+                default=F("product__price"),
+                output_field=DecimalField(max_digits=10, decimal_places=2),
+            ),
             output_field=DecimalField(max_digits=10, decimal_places=2),
-        )
+        ),
     )
 
 
@@ -46,12 +60,12 @@ def get_total_price_order(order: Order) -> Decimal:
     Calculates the total price of an order by summing up the total price of each item,
     considering quantity and any discount at the database.
     """
-    items = get_items_with_price_based_quantity(order)
+    items = _get_items_price_based_on_quantity(order)
     total = items.aggregate(total=Sum("total_item_price"))["total"]
     return total.quantize(Decimal("0.01")) if total else Decimal("0.00")
 
 
-def update_total_price_for_orders_pending(product, data):
+def update_total_price_for_orders_pending(product: Product, data: dict) -> None:
     """
     Updates the pending order items and recalculates total prices for affected orders.
     """
